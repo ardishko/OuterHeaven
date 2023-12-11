dda6fdc3ccd758d00192a965c7ab431409edf945
ardishco-the-great <ardishco@protonmail.com>
2023-12-11 18:02:23 +0300

    nvm I didn't add sex

--- home-manager/programs/packages.nix
+++ home-manager/programs/packages.nix
@@ -147,15 +147,18 @@
     gnome.pomodoro
     ytui-music
     youtube-tui
+    vinegar
     qrscan
     # Overrides
 
-    (pkgs.vinegar.overrideAttrs {
-      postInstall = ''
-        wrapProgram $out/bin/vinegar \
-        --prefix PATH : ${lib.makeBinPath [wineWowPackages.staging]}
-      '';
-    })
+    # (pkgs.vinegar.overrideAttrs {
+    #   src = fetchFromGitHub {
+    #     owner = "vinegarhq";
+    #     repo = "vinegar";
+    #     rev = "v1.5.9";
+    #     hash = "sha256-cLzQnNmQYyAIdTGygk/CNU/mxGgcgoFTg5G/0DNwpz4=";
+    #   };
+    # })
 
     (pkgs.goverlay.overrideAttrs {
       # cock
