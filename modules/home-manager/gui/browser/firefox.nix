e1cb043fc8e5cf634aee885aa39b06292f15de55
ardishco <ardishco@protonmail.com>
2024-07-30 03:31:15 +0300

    reverted firefox to stable and added SRM dirs

--- modules/home-manager/gui/browser/firefox.nix
+++ modules/home-manager/gui/browser/firefox.nix
@@ -117,7 +117,7 @@ in {
   programs = {
     firefox = {
       enable = true;
-      package = pkgs.firefox-beta;
+      package = pkgs.firefox;
       profiles = {
         "freeform" = {
           isDefault = true;
