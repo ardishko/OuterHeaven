3c0f6f490bf0ada5a9148ebde09c202abf9ed821
ardishco <ardishco@protonmail.com>
2024-08-19 22:14:19 +0300

    moved a few home related things, hope this works
    might be unstable, still not sure

--- modules/home-manager/other/home.nix
+++ modules/home-manager/other/home.nix
@@ -63,7 +63,6 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "23.05";
 
   # The dconf lines are needed for QEMU virtualization.
   dconf.settings = {
