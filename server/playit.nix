7f85b854d738309aeb83a5f380177ea79ecbace8
ardishco <ardishco@protonmail.com>
2024-08-21 03:43:29 +0300

    Changed playit's credit dir

--- server/playit.nix
+++ server/playit.nix
@@ -10,7 +10,7 @@
     wantedBy = [ "multi-user.target" ];
     after = [ "network-online.target" "systemd-resolved.service" ];
     serviceConfig = {
-      ExecStart = "${inputs.flux.packages.${pkgs.system}.playit}/bin/playit-cli --secret_path /persist/etc/playit.toml";
+      ExecStart = "${inputs.flux.packages.${pkgs.system}.playit}/bin/playit-cli --secret_path /etc/cred/playit.toml";
       Restart = "always";
       User = "playit";
       Group = "playit";
