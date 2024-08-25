a838b402df1e220d0431cb5277b858a566976cb4
ardishco <ardishco@protonmail.com>
2024-08-25 15:06:55 +0300

    Unmounted external disks for moving prep

--- machines/ShadowMoses/hardware-configuration.nix
+++ machines/ShadowMoses/hardware-configuration.nix
@@ -27,14 +27,17 @@
   #  device = "/dev/disk/by-uuid/93FD-AE0A";
   #  fsType = "vfat";
   #};
-  fileSystems."/disks/EEEEEEEEEEE" = {
-    device = "/dev/disk/by-uuid/bd44ffbb-b994-4b47-b5a2-eea71704cfbf";
-    fsType = "ext4";
-  };
-  fileSystems."/disks/1TBCP" = {
-    device = "/dev/disk/by-uuid/cee53a9e-310e-4ead-bde1-cb8ef6badc87";
-    fsType = "ext4";
-  };
+
+  # External drives
+
+  # fileSystems."/disks/EEEEEEEEEEE" = {
+  #   device = "/dev/disk/by-uuid/bd44ffbb-b994-4b47-b5a2-eea71704cfbf";
+  #   fsType = "ext4";
+  # };
+  # fileSystems."/disks/1TBCP" = {
+  #   device = "/dev/disk/by-uuid/cee53a9e-310e-4ead-bde1-cb8ef6badc87";
+  #   fsType = "ext4";
+  # };
 
   swapDevices = [ ];
 
