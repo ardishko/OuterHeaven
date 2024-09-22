a743661e058a112258de6127d6a5466e07185881
ardishco <ardishco@protonmail.com>
2024-09-23 01:56:55 +0300

    Cleanup, once again
    nixpkgs-stable input has been updated, I forgot to change the number.
    and also I removed references to the Less language, Idk why Github
    thinks I have any code in Less in my configuration

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
