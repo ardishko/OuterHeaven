1f68735bd4bb8202545ef5affadb71eeae7451b2
ardishco <ardishco@protonmail.com>
2024-09-26 00:30:34 +0300

    Fixed Bluetooth audio with wireplumber

--- modules/core/services/sound.nix
+++ modules/core/services/sound.nix
@@ -5,6 +5,16 @@
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+    wireplumber = {
+      enable = true;
+      extraConfig = {
+        disable-autoswitch = {
+        "wireplumber.settings" = {
+          "bluetooth.autoswitch-to-headset-profile" = "false";
+          };
+        };
+      };
+    };
     jack.enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
