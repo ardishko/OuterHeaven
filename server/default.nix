1647788c74f58ddf27f8acb5546985270f1ed2a5
ardishco <ardishco@protonmail.com>
2026-01-21 19:42:04 +0200

    I wasn't even importing nginx...

--- server/default.nix
+++ server/default.nix
@@ -1,15 +1,12 @@
 {
   imports = [
     ./nextcloud.nix
-    # ./palworld.nix
     ./parrot.nix
     ./cloudflare.nix
     ./vaultwarden.nix
     ./jellyfin.nix
     ./tailscale.nix
-    # ./nitter.nix
-    # ./home-assistant.nix
-    # ./minecraft
+    ./nginx.nix
     ./playit.nix
     ./immich.nix
     ./grafana.nix
