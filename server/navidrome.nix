22fea80c8e937b3f94ea2813607fe887acfb8a1f
ardishco <ardishco@protonmail.com>
2026-03-05 16:42:57 +0200

    switched the navidrome package to the one on unstable

--- server/navidrome.nix
+++ server/navidrome.nix
@@ -1,6 +1,8 @@
+{ inputs, pkgs, ... }:
 {
   services.navidrome = {
     enable = true;
+    package = inputs.unstable.legacyPackages.${pkgs.system}.navidrome;
     settings = {
       MusicFolder = "/disks/Music";
     };
