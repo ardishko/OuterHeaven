43490b144f1d278bb6e03ab7b0b7b32bb901292b
ardishco <ardishco@protonmail.com>
2026-03-08 17:42:59 +0200

    home-assistant: added trusted proxies

--- server/home-assistant.nix
+++ server/home-assistant.nix
@@ -2,7 +2,14 @@
   services.home-assistant = {
     enable = true;
     config = {
-      http.server_port = 8123;
+      http = {
+        server_port = 8123;
+        trusted_proxies = [
+          "127.0.0.1"
+          "::1"
+        ];
+        use_x_forwarded_for = true;
+      };
       homeassistant = {
         temperature_unit = "C";
         unit_system = "metric";
