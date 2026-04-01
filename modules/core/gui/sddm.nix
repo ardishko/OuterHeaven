6fb7a3ba7ea42ce61981e133c928fe58ea5289d1
ardishco <ardishco@protonmail.com>
2026-04-02 02:02:46 +0300

    Create sddm.nix

---
+++ modules/core/gui/sddm.nix
@@ -0,0 +1,38 @@
+{
+  pkgs,
+  lib,
+  # config,
+  ...
+}:
+{
+  services = {
+    displayManager = {
+      sddm = {
+        enable = true;
+        theme = "catppuccin-frappe-sky";
+        package = lib.mkForce pkgs.kdePackages.sddm;
+        wayland.enable = true;
+      };
+      # autoLogin = {
+      #   enable = if (config.networking.hostName == "Tanker") then true else false;
+      #   user = builtins.head (
+      #     builtins.attrNames (lib.filterAttrs (n: u: u.isNormalUser) config.users.users)
+      #   );
+      # };
+    };
+    xserver = {
+      displayManager = {
+        lightdm.enable = false;
+      };
+    };
+  };
+  environment.systemPackages = [
+    (pkgs.catppuccin-sddm.override {
+      flavor = "frappe";
+      accent = "sky";
+      font = "Iosevka Nerd Font";
+      fontSize = "15";
+      loginBackground = true;
+    })
+  ];
+}
