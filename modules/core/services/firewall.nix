6964fa3da727bc696f10f760d32f4545dfa04aa3
ardishco <ardishco@protonmail.com>
2026-04-20 16:44:15 +0300

    firewall.nix: opened ports for pterodactyl wings, palworld and steam's query

--- modules/core/services/firewall.nix
+++ modules/core/services/firewall.nix
@@ -10,12 +10,18 @@
             80
             443
             25565
+            8080
+            2022
+            8211
           ];
           allowedUDPPorts = [
             22
             80
             443
             25565
+            8080
+            8211
+            27015
           ];
         };
         networkmanager.enable = true;
