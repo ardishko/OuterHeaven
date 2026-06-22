3d9d980f9919dda9bd8b9d0033419017787f83f7
ardishco <ardishco@protonmail.com>
2026-06-23 00:20:46 +0300

    nix: removed garnix as it's shutting down

--- modules/core/other/nix.nix
+++ modules/core/other/nix.nix
@@ -11,7 +11,6 @@
       # substituters to use
       substituters = [
         "https://cache.nixos.org/"
-        "https://cache.garnix.io"
         "https://anyrun.cachix.org"
         "https://ezkea.cachix.org"
         "https://nix-community.cachix.org"
@@ -21,7 +20,6 @@
 
       trusted-public-keys = [
         "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
-        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
